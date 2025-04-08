c9babee8af217117d6cefff8a16b31c13107ba9d
KattarCoder <43518530+sagarpjpt@users.noreply.github.com>
2025-04-08 21:47:01 +0530

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,21 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+*/ 
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        // x ^ 0 = x and x ^ x = 0
+        int res = 0;
+        for(int num : nums)
+            res ^= num;
+        return res;
+    }
+}
